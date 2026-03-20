e4d43a65dd163591ed62103b4dca971812d05ea1
Marco Hauswirth <marco.hauswirth@auterion.com>
2026-03-20 17:03:43 +0100

    fix(ekf2): use CTRL params for AdvertiseTopics, remove dead syncGpsFc/syncEvFc

--- src/modules/ekf2/EKF2.cpp
+++ src/modules/ekf2/EKF2.cpp
@@ -270,7 +270,7 @@ void EKF2::AdvertiseTopics()
 
 #if defined(CONFIG_EKF2_BAROMETER)
 
-		if (_fc.baro.enabled) {
+		if (_param_ekf2_baro_ctrl.get()) {
 			_estimator_aid_src_baro_hgt_pub.advertise();
 			_estimator_baro_bias_pub.advertise();
 		}
@@ -352,7 +352,7 @@ void EKF2::AdvertiseTopics()
 
 #if defined(CONFIG_EKF2_OPTICAL_FLOW)
 
-		if (_fc.of.enabled) {
+		if (_param_ekf2_of_ctrl.get()) {
 			_estimator_optical_flow_vel_pub.advertise();
 			_estimator_aid_src_optical_flow_pub.advertise();
 		}
